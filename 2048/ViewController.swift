303124e7845ee88f07bc8f775c2bf219ad36c637
mjb7480 <mjb7480@rit.edu>
2018-09-28 17:04:40 +0400

    Initial Commit

---
+++ 2048/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2048
+//
+//  Created by Michael John Barton on 9/28/18.
+//  Copyright © 2018 MJBarton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
